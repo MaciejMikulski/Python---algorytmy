1fb1c5ceb31bce95caf639076f6acbd708368067
MaciejMikulski <maciej.mikulski12@gmail.com>
2025-06-10 22:06:57 +0200

    Updated marker points selection
    Unified marker points function to match the C++ one that is execuded by hardware. Added adaptive P3 max error calculation based on vector lengths.

--- visualAlgorithm.py
+++ visualAlgorithm.py
@@ -265,35 +265,44 @@ class visualAlgorithm:
         return np.array(centers).astype(int)
 
     ################################# PnP HELPER FUNCTIONS #################################
+    def _euclidean_distance(self, p1, p2):
+        dx = p1[0] - p2[0]
+        dy = p1[1] - p2[1]
+        return np.sqrt(dx * dx + dy * dy)
+
     def _findMarkerPoints(self, centers, addRandPoints=False, randPointNum=0):
         """
         This method removes returns 4 points coordinates that make up marker.
         Points are always ordered: P0, P1, P2, P3.
-
+    
         Parameters
         ----------
         centers: ndarray
             2D array containing coordinates of all blob centers detected in the image
-
+    
         Returns
         ----------
         ndarray containing coordinates of points that make up marker in order: P0, P1, P2, P3.
         If no marker is detected all points have coordinates (0,0).
-
+    
         ndarray containing image with selected points.
-
+    
         bool indicating if the algorithm was successful. 
         """
-        fourthBlobMaxDistanceError = 5.0
+        p3_max_dist_err_ratio = 0.3 # Max error in p3 distance from idal point is 30% of vector length, so anywhere inside blob
         markerPoints = np.zeros((4,2))
         success = False
+
         if addRandPoints:
             tmpCenters = self._addRandomPoints(centers, randPointNum)
         else:
             tmpCenters = centers
+
         im = imageWithPoints(tmpCenters, 120, 160)
+
         blobNum = tmpCenters.shape[0]
         tripletNum = blobNum**3 - 3*blobNum**2 + 2*blobNum # n * (n-1) * (n-2)
+
         pointTriplets = np.zeros((tripletNum,3))
         # Store indexes of other points to use them in checking which one may fit the vector base
         otherPoints = np.zeros((tripletNum,blobNum-3))
@@ -312,10 +321,13 @@ class visualAlgorithm:
         pointTriplets = pointTriplets.astype(int)
         otherPoints = otherPoints.astype(int)
 
-        # Calculate predicted placement of fourth point
-        expectedP3 = np.zeros((tripletNum, 2))
-        distanceErr = np.zeros((tripletNum, otherPoints.shape[1]))
-        crossProducts = np.zeros((tripletNum,))
+        min_distance = np.inf
+        best_triplet_index = -1
+        best_other_index = -1
+        expectedP3 = []
+        cross = 0.0
+        dist = 0.0
+
         for i in range(tripletNum):
             # Get coordinates of points from triplet triplets
             p0 = tmpCenters[pointTriplets[i, 0],:]
@@ -324,34 +336,35 @@ class visualAlgorithm:
             # Calculate base vectors
             v0 = p1 - p0
             v1 = p2 - p1
-            expectedP3[i,:] = p0 + 0.5 * (v1 - v0)
+            v2 = 0.5 * (v1 - v0)
+            expectedP3 = p0 + v2
             # Check for correct orientation of base vectors
-            tmp  = np.cross(v0,v1)
-            crossProducts[i] = tmp
+            cross  = np.cross(v0,v1)
+            if cross > 0.0:
+                continue
+            
             # Calculate distance of predicted points to all other (not in triplet) points
+            # Maximum acceptable distance from the ideal P3 location is the V2 lengt times ratio
+            p3_max_dist_err = np.linalg.norm(v2) * p3_max_dist_err_ratio
             for j in range(otherPoints.shape[1]):
-                pa = expectedP3[i,:]
-                pb = tmpCenters[otherPoints[i,j],:]
-                distanceErr[i,j] = math.sqrt((pa[0]-pb[0])**2 + (pa[1]-pb[1])**2)
-
-        # TODO: add selection based on absolute value of min error and return if it is too big
-        # Find the indices of all occurrences of the minimum value
-        if np.min(distanceErr) < fourthBlobMaxDistanceError:
-            indices = np.argwhere(distanceErr == np.min(distanceErr))
-            success = True
-        else:
-            return markerPoints.astype(int), im, success
-
-
-        # Perform final selection of marker points
-        for i in range(indices.shape[0]):
-            if crossProducts[indices[i,0]] < 0.0: # Discad mirror view of the marker
-                markerPoints[0,:] = tmpCenters[pointTriplets[indices[i,0], 0],:]
-                markerPoints[1,:] = tmpCenters[pointTriplets[indices[i,0], 1],:]
-                markerPoints[2,:] = tmpCenters[pointTriplets[indices[i,0], 2],:]
-                markerPoints[3,:] = tmpCenters[otherPoints  [indices[i,0], 0],:]
-
+                candidate_point = tmpCenters[otherPoints[i,j],:]
+                dist = self._euclidean_distance(expectedP3, candidate_point)
+                # Check if point is within acceptable distance from ideal and closest so far
+                if dist < p3_max_dist_err and dist < min_distance:
+                    min_distance = dist
+                    best_triplet_index = i
+                    best_other_index = j
+                    success = True
+   
+        if success:
+            triplet = pointTriplets[best_triplet_index, :]
+            markerPoints[0,:] = tmpCenters[triplet[0],:]
+            markerPoints[1,:] = tmpCenters[triplet[1],:]
+            markerPoints[2,:] = tmpCenters[triplet[2],:]
+            markerPoints[3,:] = tmpCenters[otherPoints[best_triplet_index, best_other_index],:]
+    
         return markerPoints.astype(int), im, success
+
     
     def _addRandomPoints(self, points, num, maxX=160, maxY=120):
         """
